55cb4fca2e70add3002b8595ffc4f77699fe1bc5
Alexey <vook-alf@yandex.ru>
2024-04-01 04:28:58 +0500

    To basic position
    To basic position

--- src/main/java/ru/practicum/shareit/exception/NotOwnerForbiddenException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class NotOwnerForbiddenException extends RuntimeException {
-    public NotOwnerForbiddenException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,14 +1,7 @@
 package ru.practicum.shareit.user;
 
-        import lombok.Builder;
-        import lombok.Data;
-
-
-@Data
-@Builder
+/**
+ * TODO Sprint add-controllers.
+ */
 public class User {
-
-    private Long id;
-    private String name;
-    private String email;
 }
