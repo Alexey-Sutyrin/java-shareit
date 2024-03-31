567cd9ee80a7cbadda14676a0a4210c3bdcf4112
Alexey <vook-alf@yandex.ru>
2024-04-01 04:35:50 +0500

    Sprint 13 - ShareIt - Iteration 1 - add-controllers - correct synchronization

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,21 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.Builder;
-import lombok.Data;
-
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-
-
-@Data
-@Builder
+/**
+ * TODO Sprint add-controllers.
+ */
 public class ItemDto {
-
-    private Long id;
-    @NotEmpty
-    private String name;
-    @NotEmpty
-    private String description;
-    @NotNull
-    private Boolean available;
 }
