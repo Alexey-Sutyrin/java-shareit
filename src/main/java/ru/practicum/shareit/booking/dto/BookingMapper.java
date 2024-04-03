d0f57526406d33854133786072a3f1e2b4465a5a
Alexey <vook-alf@yandex.ru>
2024-04-04 00:06:07 +0500

    Sprint 14 - add - bookings - Iteration 8 - BookingMapper change

--- src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
@@ -5,6 +5,7 @@ import ru.practicum.shareit.booking.Booking;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BookingMapper {
 
@@ -20,12 +21,18 @@ public class BookingMapper {
     }
 
     public static List<BookingOutDto> mapToBookingDto(Collection<Booking> bookings) {
+        return bookings.stream()
+                .map(BookingMapper::toBookingDto)
+                .collect(Collectors.toList());
+    }
+    /*старый способ убран, использованы поток stream(), метод map() и собран результат с помощью Collectors.toList()
+    public static List<BookingOutDto> mapToBookingDto(Collection<Booking> bookings) {
         List<BookingOutDto> dtos = new ArrayList<>();
         for (Booking booking : bookings) {
             dtos.add(toBookingDto(booking));
         }
         return dtos;
-    }
+    }*/
 
     public static Booking toBooking(BookingDto bookingDto) {
         Booking booking = new Booking();
